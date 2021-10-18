bc15640c6abeb52a50d724b692d74f96b66a09f6
Heraldo Saldanha <heraldo93@gmail.com>
2021-10-18 09:01:39 -0300

    Criando forma de parcelar pasando somente o tipo de rateio

---
+++ Parcelamento/Rateio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parcelamento
+{
+    public static class Rateio
+    {
+        public static SortedDictionary<int, decimal> Ratear(TipoRateio tipoRateio, decimal valor, int quantidadeParcelas)
+        {
+            SortedDictionary<int, decimal> retorno = new SortedDictionary<int, decimal>();
+
+            switch (tipoRateio)
+            {
+                case TipoRateio.RestoPrimeiraParcela:
+                    retorno = Parcelar.RestoPrimeiraParcela(valor, quantidadeParcelas);
+                    break;
+                case TipoRateio.RestoUltimaParcela:
+                    retorno = Parcelar.RestoUltimaParcela(valor, quantidadeParcelas);
+                    break;
+                case TipoRateio.RestoRateadoEntreParcelasCrescente:
+                    retorno = Parcelar.RestoRateadoEntreParcelasCrescente(valor, quantidadeParcelas);
+                    break;
+                case TipoRateio.RestoRateadoEntreParcelasDecrescente:
+                    retorno = Parcelar.RestoRateadoEntreParcelasDecrescente(valor, quantidadeParcelas);
+                    break;
+            }
+
+            return retorno;
+        }
+    }
+}
